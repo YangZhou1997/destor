4b1c350e5c94b83696fdfc4e00368d2364e4d764
administrator <administrator@admiral-ubuntu-worker.eng.vmware.com>
2020-06-07 23:48:30 +0300

    backup before touching chunking algo

--- run.sh
+++ run.sh
@@ -2,6 +2,14 @@
 
 ./rebuild ~/working/
  
-for file in vmware-distrib-0 vmware-distrib-1 vmware-distrib-2 vmware-distrib-3 vmware-distrib-4 vmware-distrib-5; do
-    ./destor -c"/home/administrator/destor/destor.config" /home/administrator/datasets/vmware-build/$file
+# for file in vmware-distrib-0 vmware-distrib-1 vmware-distrib-2 vmware-distrib-3 vmware-distrib-4 vmware-distrib-5; do
+#     ./destor -c"/home/administrator/destor/destor.config" /home/administrator/datasets/vmware-build/$file
+# done
+
+# for file in db0 db1 db2 db3 db4; do
+#     ./destor -c"/home/administrator/destor/destor.config" /home/administrator/datasets/db-files/$file
+# done
+
+for file in enwiki-20200220-abstract.xml enwiki-20200301-abstract.xml enwiki-20200401-abstract.xml enwiki-20200420-abstract.xml enwiki-20200501-abstract.xml enwiki-20200520-abstract.xml enwiki-20200601-abstract.xml enwiki-latest-abstract.xml; do
+    ./destor -c"/home/administrator/destor/destor.config" /home/administrator/datasets/wiki/$file
 done
--- speedup-scripts/wget.sh
+++ speedup-scripts/wget.sh
@@ -1,7 +1,13 @@
 # !/bin/bash
-mkdir -p ~/dataset/
-mkdir -p ~/dataset/fslhomes/
-mkdir -p ~/dataset/macos/
+# mkdir -p ~/dataset/
+# mkdir -p ~/dataset/fslhomes/
+# mkdir -p ~/dataset/macos/
 
-cd ~/dataset/fslhomes/ && wget --random-wait -r -np -e robots=off -U -R "index.html*" -c http://tracer.filesystems.org/traces/fslhomes/2012/
-cd ~/dataset/macos/ && wget --random-wait -r -np -e robots=off -U -R "index.html*" -c http://tracer.filesystems.org/traces/macos/2012/
+# cd ~/dataset/fslhomes/ && wget --random-wait -r -np -e robots=off -U -R "index.html*" -c http://tracer.filesystems.org/traces/fslhomes/2012/
+# cd ~/dataset/macos/ && wget --random-wait -r -np -e robots=off -U -R "index.html*" -c http://tracer.filesystems.org/traces/macos/2012/
+
+wiki_link=(https://dumps.wikimedia.org/enwiki/20200220/enwiki-20200220-abstract.xml.gz https://dumps.wikimedia.org/enwiki/20200301/enwiki-20200301-abstract.xml.gz https://dumps.wikimedia.org/enwiki/20200401/enwiki-20200401-abstract.xml.gz https://dumps.wikimedia.org/enwiki/20200420/enwiki-20200420-abstract.xml.gz https://dumps.wikimedia.org/enwiki/20200501/enwiki-20200501-abstract.xml.gz https://dumps.wikimedia.org/enwiki/20200520/enwiki-20200520-abstract.xml.gz https://dumps.wikimedia.org/enwiki/20200601/enwiki-20200601-abstract.xml.gz https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-abstract.xml.gz)
+
+for link in https://dumps.wikimedia.org/enwiki/20200301/enwiki-20200301-abstract.xml.gz https://dumps.wikimedia.org/enwiki/20200401/enwiki-20200401-abstract.xml.gz https://dumps.wikimedia.org/enwiki/20200420/enwiki-20200420-abstract.xml.gz https://dumps.wikimedia.org/enwiki/20200501/enwiki-20200501-abstract.xml.gz https://dumps.wikimedia.org/enwiki/20200520/enwiki-20200520-abstract.xml.gz https://dumps.wikimedia.org/enwiki/20200601/enwiki-20200601-abstract.xml.gz https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-abstract.xml.gz; do
+    cd ~/datasets/wiki && wget $link
+done
